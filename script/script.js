e82c5cbc1d5079077fe9f1a194cdd283292a236e
Khalid <faragk1@mcmaster.ca>
2023-05-20 16:11:08 -0400

    Added a new api for the more details

--- script/script.js
+++ script/script.js
@@ -3,17 +3,25 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const search = document.querySelector(".search input");
 const btn = document.querySelector(".search button");
 const icon = document.querySelector(".weather-icon");
+const countAPI = "http://api.weatherapi.com/v1/current.json?q="
+const countKey = "be464b2cfbd9441c8b8192831232005";
 
 async function getWeather(city) {
     const response = await fetch(apiURL + city + `&appid=` + apiKey);
+    const response2 = await fetch(countAPI + city + `&key=` + countKey);
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
         document.querySelector(".read-more").style.display = "none";
     } else{
+    var data2 = await response2.json();
+    console.log(data2);
     var data = await response.json();
+    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".country").innerHTML = data2.location.country;
+    document.querySelector(".feels-like").innerHTML = "Feels like: " + Math.round(data2.current.feelslike_c) + "°C";
     document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
